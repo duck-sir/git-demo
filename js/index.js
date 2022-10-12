6e6a596fde5d37b6cae5412c27cc4d22f41b99b4
fc <2394651228@qq.com>
2022-10-12 20:34:08 +0800

    add github

--- js/index.js
+++ js/index.js
@@ -4,3 +4,10 @@ console.log('l')
 console.log('l')
 console.log('o')
 // v1.0.0
+console.log('G')
+console.log('i')
+console.log('t')
+console.log('h')
+console.log('o')
+console.log('b')
+// v2.0.0
